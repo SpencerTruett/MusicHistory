8f0cf9e8dd1f9d0c1fb57a88b12ce33ce17e8034
Spencer Truett <srtruett1993@gmail.com>
2020-03-19 13:06:45 -0500

    finished exercise

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,4 +1,4 @@
-﻿SELECT * FROM Genre g
+﻿--SELECT * FROM Genre g
 
 --INSERT INTO Artist (ArtistName, YearEstablished) VALUES ('Queen', 1970)
 
@@ -12,6 +12,28 @@
 
 --INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES ('Bohemian Rhapsody', 357, '11/21/1975', 2, 28, 23)
 
---Leaving off right here, we'll pick up here tomorrow
-SELECT al.Title, al.ArtistName FROM Album al
-LEFT JOIN Artist
+--SELECT * FROM Album al
+--LEFT JOIN Artist ar ON ar.Id = al.ArtistId
+--LEFT JOIN Song s ON s.AlbumId = al.Id
+--WHERE al.Id = 23
+
+--SELECT al.Title, COUNT (s.Id) AS NumberOfSongs FROM Album al
+--LEFT JOIN Song s ON s.AlbumId = al.Id
+--GROUP BY al.Title
+
+--SELECT ar.ArtistName, COUNT (s.Id) AS NumberOfSongs FROM Artist ar
+--LEFT JOIN Song s ON s.ArtistId = ar.Id
+--GROUP BY ar.ArtistName
+
+--SELECT g.[Label], COUNT (s.Id) AS NumberOfSongs FROM Genre g
+--LEFT JOIN Song s ON s.GenreId = g.Id
+--GROUP BY g.[Label]
+
+
+--SELECT al.Title, MAX (al.AlbumLength) AS AlbumLength FROM Album al
+--GROUP BY al.Title
+--ORDER BY AlbumLength DESC;
+
+SELECT s.Title, MAX (s.SongLength) AS SongLength FROM Song s
+GROUP BY s.Title
+ORDER BY SongLength DESC;
